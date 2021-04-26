566741764070d893a3bc5ba7c5c8bfcee959aab0
androbomb <alessandro.bombini.fi@gmail.com>
2021-04-26 10:18:51 +0200

    Collider Damage with velocity (works) v1

--- Assets/Scripts/ColliderDamager.cs
+++ Assets/Scripts/ColliderDamager.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class ColliderDamager : MonoBehaviour
 {
+    #region public Objects
     public float Damage = -10.0f;
+    #endregion
+
+    #region private Objects
+
+    #endregion
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Health playerHealth = collision.gameObject.GetComponent<Health>();
         if(playerHealth != null)
         {
-            Debug.Log("UPDATECALLED");
-            playerHealth.ChangeHealth(Damage);
+            //Debug.Log("UPDATECALLED");
+            //playerHealth.ChangeHealth(Damage);
+            playerHealth.ChangeHealth(
+                ComputeCollisionDamage(collision)
+            );
+        }
+    }
+
+    private float ComputeCollisionDamage(Collision2D collision)
+    {
+        Rigidbody2D _player_rigid_body = collision.gameObject.GetComponent<Rigidbody2D>();
+        Vector2 _player_velocity = _player_rigid_body.velocity;
+        if (_player_velocity != null)
+        {
+            float _damage = Damage*_player_velocity.magnitude;
+            //Debug.Log("Damage! it is with velocity, ad it is " + _damage);
+            return _damage; 
+        } else
+        {
+            //Debug.Log("Damage! it is without velocity");
+            return Damage;
         }
     }
 }
